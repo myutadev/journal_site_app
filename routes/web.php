79bbcc8a97b41c935d539997ac13f9ab2e6c1310
myuta <myuta.dev@gmail.com>
2023-10-29 17:51:20 +0900

    change route for /

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ArticleController::class, 'index']);
 
 Route::resource("articles", ArticleController::class);
